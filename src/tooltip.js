49cd8d136644af679b2786958d63e52b64aff631
Gregory STOOS <gregory.stoos@rupeal.com>
2016-07-22 10:00:30 +0100

    added content target and firts behaviors on click

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
 export default class Tooltip extends React.Component {
+    construct(props){
+        super(props);
+
+        this.state = {
+            tootltipVisible: false
+        };
+    }
+
+    componentDidMount(){
+        this.props.refs.target.addEventListener('click', this.toggleTootltip);
+    }
+    componentDidUnMount(){
+        this.props.refs.target.removeEventListener('click', this.toggleTootltip);
+    }
+
+    toggleTootltip(){
+        this.setState({tootltipVisible: !this.state.tootltipVisible});
+    }
+
     render () {
         return (
             <div className={this.props.className}>
-                test
+                <div ref="target" className="Tooltip-target">{this.props.children}</div>
+                <div 
+                    className="Tooltip-content" 
+                    style={{display:(this.state.tootltipVisible)?'block':'none'}}
+                >
+                    {this.props.content}
+                </div>
             </div>
         );
     }
