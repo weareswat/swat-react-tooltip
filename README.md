e4aa95b85a1ddc3ab5b10a1e03d70a11578786b3
Gregory STOOS <gregory.stoos@rupeal.com>
2016-07-22 09:39:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# swat-react-tooltip
---
+++ src/tooltip.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Tooltip extends React.Component {
+    render () {
+        return (
+            <div className={this.props.className}>
+                test
+            </div>
+        );
+    }
+}
